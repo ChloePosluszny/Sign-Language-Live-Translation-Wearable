12e6af4b84e18872a301046b52ccbd14943e8e3e
David Cassity <davitrton123@gmail.com>
2025-01-28 16:08:37 -0600

    Activated BT Capabilities
    - Rewrote some of the functions to be more efficient
    - Created myPrint and myPrintln to switch between printing to Serial Monitor or Serial over Bluetooth
    - Global flag can be set false or true

---
+++ integrated/integrated.ino
@@ -0,0 +1,130 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include "BluetoothSerial.h"
+
+const bool BLUETOOTH = true; // Set to true to enable Bluetooth output
+
+BluetoothSerial SerialBT;
+Adafruit_MPU6050 mpu;
+
+void myPrint(const String &message) {
+  if (BLUETOOTH) {
+    SerialBT.print(message);
+  } else {
+    Serial.print(message);
+  }
+}
+
+void myPrintln(const String &message = "") {
+  if (BLUETOOTH) {
+    SerialBT.println(message);
+  } else {
+    Serial.println(message);
+  }
+}
+
+const int flexSensorPins[5] = {26, 27, 14, 12, 13}; // Flex sensor pins
+const int hallSensorPins[5] = {34, 35, 32, 33, 25}; // Hall sensor pins
+int hallSensorValues[5];                            // Store Hall sensor values
+int flexSensorValues[5];
+
+void setup() {
+  Serial.begin(115200);            // Initialize Serial communication
+  if (BLUETOOTH) SerialBT.begin("ESP32"); // Start Bluetooth with a device name
+  delay(100);
+  myPrintln("Bluetooth Device is Ready to Pair");
+  
+  for (int i = 0; i < 5; i++) {
+    pinMode(hallSensorPins[i], INPUT);  // Set Hall sensor pins as input
+    pinMode(flexSensorPins[i], INPUT); // Set Flex sensor pins as input
+  }
+
+  myPrintln("Adafruit MPU6050 test!");
+
+  // Initialize MPU6050
+  if (!mpu.begin()) {
+    myPrintln("Failed to find MPU6050 chip");
+    while (1) delay(10);
+  }
+  myPrintln("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  myPrint("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+    case MPU6050_RANGE_2_G: myPrintln("+-2G"); break;
+    case MPU6050_RANGE_4_G: myPrintln("+-4G"); break;
+    case MPU6050_RANGE_8_G: myPrintln("+-8G"); break;
+    case MPU6050_RANGE_16_G: myPrintln("+-16G"); break;
+  }
+
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  myPrint("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+    case MPU6050_RANGE_250_DEG: myPrintln("+- 250 deg/s"); break;
+    case MPU6050_RANGE_500_DEG: myPrintln("+- 500 deg/s"); break;
+    case MPU6050_RANGE_1000_DEG: myPrintln("+- 1000 deg/s"); break;
+    case MPU6050_RANGE_2000_DEG: myPrintln("+- 2000 deg/s"); break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  myPrintln("Filter bandwidth set to: 21 Hz");
+
+  delay(100);
+}
+
+int getSensorData(int pin) {
+  float sensorData = analogRead(pin);
+  float sensorDataPercent = int(((sensorData - 2900) / (4095 - 2900)) * 100) / float(100);
+  sensorDataPercent = (1 - sensorDataPercent) * 100;
+  return sensorDataPercent;
+}
+
+void loop() {
+  // Read Hall sensor values
+  for (int i = 0; i < 5; i++) {
+    hallSensorValues[i] = analogRead(hallSensorPins[i]);
+  }
+
+  myPrint("Analog Sensor Values: [");
+  for (int i = 0; i < 5; i++) {
+    myPrint(String(hallSensorValues[i]));
+    if (i < 4) myPrint(", ");
+  }
+  myPrintln("]");
+
+  // Read MPU6050 data
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  myPrint("Acceleration X: ");
+  myPrint(String(a.acceleration.x));
+  myPrint(", Y: ");
+  myPrint(String(a.acceleration.y));
+  myPrint(", Z: ");
+  myPrintln(String(a.acceleration.z));
+
+  myPrint("Rotation X: ");
+  myPrint(String(g.gyro.x));
+  myPrint(", Y: ");
+  myPrint(String(g.gyro.y));
+  myPrint(", Z: ");
+  myPrintln(String(g.gyro.z));
+
+  // Read Flex sensor values
+  for (int i = 0; i < 5; i++) {
+    myPrint("Finger ");
+    switch (i) {
+      case 0: myPrint("Thumb "); break;
+      case 1: myPrint("Index "); break;
+      case 2: myPrint("Middle "); break;
+      case 3: myPrint("Ring "); break;
+      case 4: myPrint("Pinkie "); break;
+      default: myPrint("Unknown ");
+    }
+    flexSensorValues[i] = getSensorData(flexSensorPins[i]);
+    myPrintln("Bent Percent: " + String(flexSensorValues[i]) + "%");
+  }
+
+  delay(500); // Wait for 500ms
+}
