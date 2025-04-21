1b0d37b8b4db089066638dcb7dce969ab4920435
David Cassity <davitrton123@gmail.com>
2025-04-21 14:18:40 -0500

    trained the right glove

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 trainer_names = {'ALFONSO': 'a', 'CHLOE': 'c', 'DAVID': 'd', 'ERIK': 'e', 'RAHMAN': 'r', 'TEST': 't'}
 
-TRAINING_MODE = True       # True: training (write to CSV), False: output (ML inference)
-TRAINER_NAME = 'TEST'
+TRAINING_MODE = False       # True: training (write to CSV), False: output (ML inference)
+TRAINER_NAME = 'DAVID'
 COMM_MODE = "SERIAL"       # Options: "BLUETOOTH" or "SERIAL"
-GLOVE_MODE = "DOUBLE"         # Options: "DOUBLE" (expect 2 arrays) or "SINGLE" (expect 1 array)
-HAND = "D"
+GLOVE_MODE = "SINGLE"         # Options: "DOUBLE" (expect 2 arrays) or "SINGLE" (expect 1 array)
+HAND = "R"
 
 COMMON_PATH = os.path.join("Models", trainer_names[TRAINER_NAME])
 
