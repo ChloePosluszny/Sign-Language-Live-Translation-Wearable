c71c85f3212e28492dbf9520919d047ba73dcbd0
David Cassity <davitrton123@gmail.com>
2025-04-23 19:17:08 -0500

    trained more signs

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 trainer_names = {'ALFONSO': 'a', 'CHLOE': 'c', 'DAVID': 'd', 'ERIK': 'e', 'RAHMAN': 'r', 'TEST': 't'}
 
-TRAINING_MODE = False       # True: training (write to CSV), False: output (ML inference)
+TRAINING_MODE = True       # True: training (write to CSV), False: output (ML inference)
 TRAINER_NAME = 'DAVID'
 COMM_MODE = "SERIAL"       # Options: "BLUETOOTH" or "SERIAL"
 GLOVE_MODE = "SINGLE"         # Options: "DOUBLE" (expect 2 arrays) or "SINGLE" (expect 1 array)
